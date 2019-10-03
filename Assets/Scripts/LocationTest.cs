77a54d34b1a1176414db439404a0d81014d92df4
tyanio0627 <tyanio_0627@yahoo.co.jp>
2019-10-03 16:33:03 +0900

    位置情報オン

---
+++ Assets/Scripts/LocationTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("false");
+            return;
+        }
+            
+          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
