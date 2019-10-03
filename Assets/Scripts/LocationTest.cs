af55cc864ca192f88ce45fe19db2481452810df1
tyanio0627 <tyanio_0627@yahoo.co.jp>
2019-10-03 17:16:43 +0900

    test build

--- Assets/Scripts/LocationTest.cs
+++ Assets/Scripts/LocationTest.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LocationTest : MonoBehaviour
 {
+    [SerializeField] Text testText;
+
     // Start is called before the first frame update
     void Start()
     {
         if (!Input.location.isEnabledByUser)
         {
+            testText.text = "false";
             Debug.Log("false");
             return;
         }
-            
-          
+
+        testText.text = "true";
     }
 
     // Update is called once per frame
