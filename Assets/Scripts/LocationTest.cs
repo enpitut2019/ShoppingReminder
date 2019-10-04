b27a36d3f5ac1f14df34843eecb21cf490580d6e
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2019-10-04 19:24:28 +0900

    modify LocationTest

--- Assets/Scripts/LocationTest.cs
+++ Assets/Scripts/LocationTest.cs
@@ -7,22 +7,45 @@ public class LocationTest : MonoBehaviour
 {
     [SerializeField] Text testText;
 
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
+        //端末で位置情報使用許可がおりていない
         if (!Input.location.isEnabledByUser)
         {
-            testText.text = "false";
-            Debug.Log("false");
-            return;
+            testText.text = "permission error";
+            yield break;
         }
-
-        testText.text = "true";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //位置情報取得開始
+        Input.location.Start();
+        int maxWait = 20;
+        //位置情報取得準備中
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            testText.text = "searching...";
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+        //タイムアウト
+        if (maxWait < 1)
+        {
+            testText.text = "Timed out";
+            print("Timed out");
+            yield break;
+        }
+        //位置情報取得不可
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            testText.text = "Unable to determine device location";
+            print("Unable to determine device location");
+            yield break;
+        }
+        //位置情報取得完了
+        else
+        {
+            testText.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+            //print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+        //位置情報取得終了
+        Input.location.Stop();
     }
 }
