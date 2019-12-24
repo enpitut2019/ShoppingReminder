df99e80008c94dfe2c93d93f149b682c7928a556
tyanio0627 <tyanio_0627@yahoo.co.jp>
2019-12-24 18:33:34 +0900

    データベースのバージョンを更新しました

--- app/src/main/java/com/ji/shoppingreminder/database/RequisiteDataBaseBuilder.java
+++ app/src/main/java/com/ji/shoppingreminder/database/RequisiteDataBaseBuilder.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class RequisiteDataBaseBuilder extends SQLiteOpenHelper {
 
     // データーベースのバージョン
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     // データーベース名
     public static final String DATABASE_NAME = "RequisiteDB.db";
     private static final String TABLE_NAME = "requisitedb";
