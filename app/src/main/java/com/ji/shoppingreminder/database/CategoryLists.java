8be64eca011c7e588647ed4f3a87981fe9c8443d
tyanio0627 <tyanio_0627@yahoo.co.jp>
2020-01-14 17:41:00 +0900

    通知するカテゴリを増やしました

--- app/src/main/java/com/ji/shoppingreminder/database/CategoryLists.java
+++ app/src/main/java/com/ji/shoppingreminder/database/CategoryLists.java
@@ -8,11 +8,13 @@ public class CategoryLists {
     public List<String> foodStore = new ArrayList<String>(){
         {
             add("SUPERMARKET");
+            add("SHOPPING_MALL");
         }
     };
 
     public List<String> groceryStore = new ArrayList<String>(){
         {
+            add("CONVENIENCE_STORE");
             add("GROCERY_OR_SUPERMARKET ");
             add("SUPERMARKET");
             add("PHARMACY");
@@ -24,6 +26,7 @@ public class CategoryLists {
             add("CLOTHING_STORE");
             add("SHOPPING_MALL");
             add("SHOE_STORE");
+            add("DEPARTMENT_STORE");
         }
     };
 }
